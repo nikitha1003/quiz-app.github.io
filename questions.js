5c86db66e0fd6941db58944b49fe14e22423cc4f
nikitha1003 <146569126+nikitha1003@users.noreply.github.com>
2023-10-24 21:21:48 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,94 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+      numb: 1,
+      question: "Which country is famous for the dish sushi?",
+      answer: "Japan",
+      options: [
+        "Italy",
+        "Japan",
+        "Mexico",
+        "India"
+      ]
+    },
+      {
+      numb: 2,
+      question: "What is the largest desert in the world?",
+      answer: "Antarctic Desert",
+      options: [
+        "Sahara Desert",
+        "Gobi Desert",
+        "Arabian Desert",
+        "Antarctic Desert"
+      ]
+    },
+    {
+      numb: 3,
+      question: "In which year did the Titanic sink?",
+      answer: "1912",
+      options: [
+        "1910",
+        "1914",
+        "1912",
+        "1915"
+      ]
+    },
+      {
+      numb: 4,
+      question: "What does CSS stand for?",
+      answer: "Cascading Style Sheet",
+      options: [
+        "Common Style Sheet",
+        "Colorful Style Sheet",
+        "Computer Style Sheet",
+        "Cascading Style Sheet"
+      ]
+    },
+    {
+        numb: 5,
+        question: "Who painted the famous artwork Starry Night?",
+        answer: "Vincent van Gogh",
+        options: [
+          "Claude Monet",
+          "Leonardo da Vinci",
+          "Vincent van Gogh",
+          "Pablo Picasso"
+        ]
+      } ,
+      {
+        numb: 6,
+        question: "Which Fisher brother struggles with his feelings for Belly throughout the series?",
+        answer: "Conrad",
+        options: [
+          "Jeremiah",
+          "Conrad",
+          "Steven",
+          "Taylor"
+        ]
+      } ,
+      {
+        numb: 7,
+        question: "Which Taylor Swift album features the song Love Story?",
+        answer: "Fearless",
+        options: [
+            "Speak Now",
+          "Fearless",
+          "Red",
+          "1989"
+        ]
+      } ,
+      {
+        numb: 8,
+        question: "Who is known as the Father of Modern Physics?",
+        answer: " Albert Einstein",
+        options: [
+            " Isaac Newton",
+          " Albert Einstein",
+          "  Galileo Galilei",
+          " Niels Bohr"
+        ]
+      }  
+
+
+
+  ];
